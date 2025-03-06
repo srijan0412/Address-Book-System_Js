f3744425a2683231b50a2c6c63c31aca4b97a927
srijan0412 <srijan.soni01@gmail.com>
2025-03-06 16:52:53 +0530

    [Srijan] Ability to search and delete a record by name

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -1,20 +1,23 @@
 const AddressBook = require('./Source/Service/AddressBook');
 const Contact = require('./Source/Model/Contact');
+
 const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Ankit", "Patel", "Bhopal", "India", "In", "100001", "9876543210", "yugap1806@gmail.com"
+        "Alice", "Mathew", "Mumbai", "India", "In", "100001", "8495784598", "alice@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
+        "Bob", "Perry", "Delhi", "India","In","1000000","5689784556","bob@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
-    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log(addressBook.editContact("John", { address: "Sample Address", city: "New Delhi", phone: "9854785698" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
+    console.log(addressBook.deleteContact("John"));
+    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
