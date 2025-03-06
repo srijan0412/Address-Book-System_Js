baad1cbaf1ce6ef166574bb55fb824d83780e09c
srijan0412 <srijan.soni01@gmail.com>
2025-03-06 16:40:21 +0530

    [Srijan] Add . Ability to create a New Address Book array and add new Contacts to it

--- src/AddressBookMain.js
+++ src/AddressBookMain.js
@@ -3,8 +3,25 @@ const Contact = require('./Source/Model/Contact');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact(
-    "Bob", "Tate", "Sector-11 Bombay Street", "Bhopal", "India", "462022", "9876543210", "yugap1806@gmail.com"
-);
-console.log(addressBook.addContact(contact1));
-console.log(addressBook.getAllContacts());
+try {
+    const contact1 = new Contact(
+        "Ankit", "Patel", "Bhopal", "India", "In", "100001", "9876543210", "yugap1806@gmail.com"
+    );
+    console.log(addressBook.addContact(contact1));
+    const contact2 = new Contact(
+        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
+    )
+    console.log(addressBook.addContact(contact2)); 
+    console.log(addressBook.getAllContacts());
+} catch (error) {
+    console.error("Error:", error.message);
+}
+
+try {
+    const invalidContact = new Contact(
+        "an", "son", "abc", "In", "I", "100A01", "12345", "invalid-email"
+    );
+    console.log(addressBook.addContact(invalidContact));
+} catch (error) {
+    console.error("Error:", error.message);
+}
