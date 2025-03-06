d5dbf2667058e1ebd0d7ce5b1df36144dd8b27d4
srijan0412 <srijan.soni01@gmail.com>
2025-03-06 16:44:09 +0530

    [Srijan] Ability to find existing contact person using name and edit it

--- AddressBookMain.js
+++ AddressBookMain.js
@@ -1,18 +1,20 @@
 const AddressBook = require('./Source/Service/AddressBook');
 const Contact = require('./Source/Model/Contact');
-
 const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
         "Ankit", "Patel", "Bhopal", "India", "In", "100001", "9876543210", "yugap1806@gmail.com"
     );
-    console.log(addressBook.addContact(contact1));
+    console.log(addressBook.addContact(contact1)); 
+
     const contact2 = new Contact(
         "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
-    console.log(addressBook.getAllContacts());
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
